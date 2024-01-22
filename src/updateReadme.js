f7bc46a09b0985bd583dcf4b675c88a0c167e097
itsjustshubh <86682116+itsjustshubh@users.noreply.github.com>
2024-01-21 19:32:39 -0500

    Updated updateReadme.js

--- src/updateReadme.js
+++ src/updateReadme.js
@@ -91,7 +91,7 @@ function updateReadme() {
     const educationContent = generateExpandableMarkdown('Education', education, formatEducation);
     const skillsContent = `<p align="center">${skills.map(formatSkill).join(' ')}</p>`;
     const projectsContent = generateExpandableMarkdown('Projects', projects, formatProject);
-    const socialProfilesContent = socialProfiles.map(socialProfiles).join('');
+    const socialProfilesContent = socialProfiles.map(formatSocialProfiles).join('');
 
     const updatedContent = templateContent
         .replace('<!-- DYNAMIC_EDUCATION -->', educationContent)
