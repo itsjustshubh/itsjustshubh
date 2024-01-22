f094678f0675c6a8a49d7d78f4dd1a26c5ea53c1
itsjustshubh <86682116+itsjustshubh@users.noreply.github.com>
2024-01-21 19:52:42 -0500

    Final updateReadme.js

--- src/updateReadme.js
+++ src/updateReadme.js
@@ -59,19 +59,19 @@ function formatProject(project) {
 }
 
 function formatSocialProfiles(profiles) {
-    let profilesMarkup = `<code><div style="display: flex; align-items: center; justify-content: center; flex-wrap: wrap;">`;
+    let profilesMarkup = `<div style="display: flex; align-items: center; justify-content: center; flex-wrap: wrap;">`;
 
     profiles.forEach(profile => {
         const iconName = path.basename(profile.icon);
         const iconPath = `src/assets/imported-icons/${iconName}`; // Assuming the extension is .svg
 
-        profilesMarkup += `<a href="${profile.link}" target="_blank" title="${profile.name}" style="background-color: ${profile.backgroundColor}; padding: 5px; margin: 5px; border-radius: 4px; display: flex; align-items: center;">
+        profilesMarkup += `<code><a href="${profile.link}" target="_blank" title="${profile.name}" style="background-color: ${profile.backgroundColor}; padding: 5px; margin: 5px; border-radius: 4px; display: flex; align-items: center;">
             <img src="${iconPath}" height="30" alt="${profile.name}" style="margin-right: 5px;">
             <strong style="font-size: 16px;">${profile.name}</strong>
-        </a>`;
+        </a></code>`;
     });
 
-    profilesMarkup += `</div></code>`;
+    profilesMarkup += `</div>`;
 
     return profilesMarkup;
 }
