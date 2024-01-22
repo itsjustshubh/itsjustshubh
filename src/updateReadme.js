54b9becdff49f590ebd90f790bdd98d14c48c576
itsjustshubh <86682116+itsjustshubh@users.noreply.github.com>
2024-01-21 20:36:09 -0500

    Final updateReadme.js

--- src/updateReadme.js
+++ src/updateReadme.js
@@ -68,7 +68,7 @@ function formatSocialProfiles(profiles) {
 
         profilesMarkup += `<a href="${profile.link}" target="_blank" title="${profile.name}" style="background-color: ${profile.backgroundColor}; padding: 5px; margin: 5px; border-radius: 4px; display: flex; align-items: center; color: ${textColor};">
             <img src="${iconPath}" height="35" alt="${profile.name}" style="margin-right: 5px;">
-            <strong style="font-size: 30px;">${profile.name}</strong>
+<!--            <strong style="font-size: 30px;">${profile.name}</strong>-->
         </a>`;
     });
 
