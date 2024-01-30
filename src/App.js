ec60b6ba9e2f7f4d733278cc5dd8e400c0d4369b
itsjustshubh <86682116+itsjustshubh@users.noreply.github.com>
2024-01-30 06:14:49 -0500

    Fixed Speed Insights

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Layout from "./pages/Layout/Layout";
 import Home from './pages/Home/Home';
 import Contact from './pages/Contact/Contact';
 import Education from './pages/Education/Education';
 import Projects from './pages/Projects/Projects';
 import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import {SpeedInsights} from "@vercel/speed-insights/react";
 
 import Edith from './prototypes/Edith/Edith';
 
 function App() {
-  const [loaded, setLoaded] = useState(false);
+    const [loaded, setLoaded] = useState(false);
 
-  useEffect(() => {
-    // Check if the page has been loaded before
-    const pageLoadedBefore = localStorage.getItem('pageLoaded');
-    if (pageLoadedBefore) {
-      setLoaded(true);
-    } else {
-      localStorage.setItem('pageLoaded', 'true');
-    }
-  }, []);
+    useEffect(() => {
+        // Check if the page has been loaded before
+        const pageLoadedBefore = localStorage.getItem('pageLoaded');
+        if (pageLoadedBefore) {
+            setLoaded(true);
+        } else {
+            localStorage.setItem('pageLoaded', 'true');
+        }
+    }, []);
 
-  return (
-    <Router>
-    <Layout>
-      <Routes>
-          {/* Main page routers */}
-          <Route path="/" element={<Home loaded={!loaded} />} />
-          <Route path="/education" element={<Education />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="*" element={<NotFoundPage />} />
+    return (
+        <Router>
+            <Layout>
+                <Routes>
+                    {/* Main page routers */}
+                    <Route path="/" element={<Home loaded={!loaded}/>}/>
+                    <Route path="/education" element={<Education/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                    <Route path="/projects" element={<Projects/>}/>
+                    <Route path="*" element={<NotFoundPage/>}/>
 
-          {/* Demo page routers */}
-          <Route path="/edith-prototype" element={<Edith />} />
-        </Routes>
-    </Layout>
-  </Router>
-  );
+                    {/* Demo page routers */}
+                    <Route path="/edith-prototype" element={<Edith/>}/>
+                </Routes>
+            </Layout>
+            <SpeedInsights/>
+        </Router>
+    );
 }
 
 export default App;
