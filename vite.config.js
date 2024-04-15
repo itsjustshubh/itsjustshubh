84e4a65e088351ae95c97378973ff2a8fe3db63c
itsjustshubh <86682116+itsjustshubh@users.noreply.github.com>
2024-04-15 04:03:44 -0400

    Added vercel.json and modified vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // Make sure base is set correctly, if needed
   build: {
     rollupOptions: {
-      input: {
-        main: "index.html",
-      },
+      input: "index.html",
     },
   },
 });
