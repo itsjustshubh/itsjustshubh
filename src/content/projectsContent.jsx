352c4a65175e27a8da123cfb4e8762968366069d
itsjustshubh <86682116+itsjustshubh@users.noreply.github.com>
2024-04-16 06:00:11 -0400

    Fixed marqueeText

--- src/content/projectsContent.jsx
+++ src/content/projectsContent.jsx
@@ -1,64 +1,56 @@
 // projectContent.js
-import ProjectImg1 from "../assets/images/projects/project-1.jpg";
-import ProjectImg2 from "../assets/images/projects/project-2.jpg";
-import ProjectImg3 from "../assets/images/projects/project-3.jpg";
-import ProjectImg4 from "../assets/images/projects/project-4.jpg";
-import ProjectImg5 from "../assets/images/projects/project-5.jpg";
-
 import { project as edith } from "./projects/edith";
-
-import { MetaLogo, XLogo } from "@phosphor-icons/react";
 
 export const projects = [
   edith,
-  {
-    id: 2,
-    slug: "social-cloud",
-    title: "Social Cloud",
-    subtitle: "Data Science • Social Marketing",
-    image: ProjectImg1,
-    link: "/projects/social-cloud",
-    content: {
-      title:
-        "Revolutionizing NGO fundraising through advanced data analytics and targeted social marketing.",
-      year: 2021,
-      credits: [
-        { role: "Founder and CEO", name: "Shubh Thorat" },
-        { role: "Data Analytics", name: "Shubh Thorat" },
-        { role: "Marketing Strategy", name: "Shubh Thorat" },
-      ],
-      goals: [
-        "Utilize big data to maximize fundraising impact.",
-        "Implement innovative social marketing strategies.",
-      ],
-      description: `
-        <p>As the founder of Social Cloud, I spearheaded the development of a platform that transforms how NGOs connect with donors. By leveraging big data, Social Cloud targets customized online audiences, maximizing the effectiveness of fundraising campaigns.</p>
-        <br />
-        <p>The platform integrates cutting-edge data analytics to optimize donor engagement and enhance the fundraising process, thereby establishing a new standard in digital social marketing for NGOs.</p>
-      `,
-      buttons: [
-        {
-          id: 1,
-          icon: MetaLogo,
-          link: "https://www.facebook.com/",
-          iconProps: {
-            size: 18,
-            weight: "light",
-            color: "#fff",
-          },
-        },
-        {
-          id: 2,
-          icon: XLogo,
-          link: "https://www.example.com/",
-          iconProps: {
-            size: 18,
-            weight: "light",
-            color: "#fff",
-          },
-        },
-      ],
-      images: [ProjectImg1, ProjectImg2, ProjectImg3, ProjectImg4, ProjectImg5],
-    },
-  },
+  // {
+  //   id: 2,
+  //   slug: "social-cloud",
+  //   title: "Social Cloud",
+  //   subtitle: "Data Science • Social Marketing",
+  //   image: ProjectImg1,
+  //   link: "/projects/social-cloud",
+  //   content: {
+  //     title:
+  //       "Revolutionizing NGO fundraising through advanced data analytics and targeted social marketing.",
+  //     year: 2021,
+  //     credits: [
+  //       { role: "Founder and CEO", name: "Shubh Thorat" },
+  //       { role: "Data Analytics", name: "Shubh Thorat" },
+  //       { role: "Marketing Strategy", name: "Shubh Thorat" },
+  //     ],
+  //     goals: [
+  //       "Utilize big data to maximize fundraising impact.",
+  //       "Implement innovative social marketing strategies.",
+  //     ],
+  //     description: `
+  //       <p>As the founder of Social Cloud, I spearheaded the development of a platform that transforms how NGOs connect with donors. By leveraging big data, Social Cloud targets customized online audiences, maximizing the effectiveness of fundraising campaigns.</p>
+  //       <br />
+  //       <p>The platform integrates cutting-edge data analytics to optimize donor engagement and enhance the fundraising process, thereby establishing a new standard in digital social marketing for NGOs.</p>
+  //     `,
+  //     buttons: [
+  //       {
+  //         id: 1,
+  //         icon: MetaLogo,
+  //         link: "https://www.facebook.com/",
+  //         iconProps: {
+  //           size: 18,
+  //           weight: "light",
+  //           color: "#fff",
+  //         },
+  //       },
+  //       {
+  //         id: 2,
+  //         icon: XLogo,
+  //         link: "https://www.example.com/",
+  //         iconProps: {
+  //           size: 18,
+  //           weight: "light",
+  //           color: "#fff",
+  //         },
+  //       },
+  //     ],
+  //     images: [ProjectImg1, ProjectImg2, ProjectImg3, ProjectImg4, ProjectImg5],
+  //   },
+  // },
 ];
